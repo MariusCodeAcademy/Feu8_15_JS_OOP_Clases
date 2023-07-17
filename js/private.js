32e0fe03f078df6ffc48b1c3aa4830cc5618b46a
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-17 10:57:27 +0300

    changeHourlyPay

--- js/private.js
+++ js/private.js
@@ -1,2 +1,32 @@
 'use strict';
 console.log('private.js file was loaded');
+
+class MyWorker {
+  firstName;
+  #hourlyPay;
+  // privati savybe
+  #hoursWorked;
+  constructor(fName, dollarPerHour) {
+    this.firstName = fName;
+    this.#hourlyPay = dollarPerHour;
+    this.#hoursWorked = 0;
+  }
+
+  changeHourlyPay(password, newPay) {
+    if (password === '123') {
+      this.#hourlyPay = newPay;
+      console.log('hourly pay was changed');
+    } else {
+      console.warn('wrong password');
+    }
+  }
+}
+
+const w1 = new MyWorker('James', 50);
+// is klases isores privacios savybes nepasiekiamos
+// w1.#hourlyPay = 100;
+// w1.#hoursWorked = 100;
+
+w1.changeHourlyPay('123', 75);
+
+console.log('w1 ===', w1);
