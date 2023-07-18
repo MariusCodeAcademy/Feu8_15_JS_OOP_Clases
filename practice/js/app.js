474479885eb348219117db53e41d93145040fdc2
Marius CA <mariuskcodeacademy@gmail.com>
2023-07-18 08:31:25 +0300

    draw

--- practice/js/app.js
+++ practice/js/app.js
@@ -1,6 +1,7 @@
 import Employee from './class/Employee.js';
 import MyWorker from './class/MyWorker.js';
 import Job from './class/Job.js';
+import Freelancer from './class/Freelancer.js';
 console.log('app.js file was loaded');
 
 const em1 = new Employee('James', 'Bond');
@@ -12,3 +13,16 @@ w1.getFullName();
 w1.work(10);
 w1.work(12);
 w1.calcPay();
+
+const j1 = new Job('Header css', 100);
+console.log('j1 ===', j1);
+const jArr = [
+  new Job('Footer', 250), // 0
+  new Job('Aside', 150), // 1
+  new Job('Main', 500), // 2
+  new Job('Slider', 600), // 3
+];
+console.log('jArr ===', jArr);
+
+const f1 = new Freelancer('Mike', 'T');
+console.log('f1 ===', f1);
